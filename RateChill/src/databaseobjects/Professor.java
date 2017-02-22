3de6e72bfe4f035d5d0d417fc95b51c35b685d0d
Ine <inelofthus@gmail.com>
2017-02-22 15:44:00 +0100

    Created and finished Professor class

--- RateChill/src/databaseobjects/Professor.java
+++ RateChill/src/databaseobjects/Professor.java
@@ -1,20 +1,46 @@
 package databaseobjects;
-
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 
 public class Professor extends DatabaseUser {
 	String username;
 	ArrayList<String> courses;
 	
-	public Professor() {
-		// TODO Auto-generated constructor stub
-		
+	public Professor(String username) {
+		this.username = username;
+		loadInfo();	
 	}
 	
 	public boolean existsInDB(){
 		return DBC.professorExists(username);
 	}
 	
+	public void loadInfo(){
+		try {
+			courses = DBC.getCoursesTaughtByProfessor(username);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			if (!existsInDB()) {
+				throw new NoSuchElementException("No courses exists for this professor");
+			}
+			System.out.println(e.getMessage());
+		}
+	}
 
+	public String getUsername() {
+		return username;
+	}
+
+	public ArrayList<String> getCourses() {
+		return courses;
+	}
+	
+	public static void main(String[] args) {
+		Professor prof = new Professor("pekkaa");
+		System.out.println(prof.getCourses());
+		System.out.println(prof.getUsername());
+	}
+	
 }
