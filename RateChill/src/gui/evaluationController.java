ad13424c174820b714d149564e64141fbd0c9704
Nicolai <nicolai_m404@hotmail.com>
2017-02-22 12:42:43 +0100

    started evaluation method, made toggle buttons like radio buttons

--- RateChill/src/gui/evaluationController.java
+++ RateChill/src/gui/evaluationController.java
@@ -4,8 +4,10 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javax.swing.ButtonModel;
-
+import database.*;
+import javafx.application.Platform;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -13,43 +15,34 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.text.Text;
 
 public class evaluationController implements Initializable {
 
 	
 	@FXML
-	TextArea feedback; 
-	@FXML
-	ToggleButton tooSlow;
-	@FXML
-	ToggleButton confusing;
-	@FXML
-	ToggleButton perfect;
-	@FXML
-	ToggleButton toofast;
-	@FXML
-	ToggleButton ok;
-	@FXML
-	Button submit;
+	public TextArea feedback; 
+	public ToggleButton tooSlow;
+	public ToggleButton confusing;
+	public ToggleButton perfect;
+	public ToggleButton toofast;
+	public ToggleButton ok;
+	public ToggleGroup group;
+	public Button submit;
+	public Text debugText;
 	
 	
-	/*
-	 final ToggleGroup group = new ToggleGroup();
-
-     
-     addListener(new ChangeListener<Toggle>() {
-                 public void changed(ObservableValue<? extends Toggle> ov,Toggle oldToggle, Toggle newToggle) {
-                     if (null != newToggle) {
-                         System.out.println(group.getSelectedToggle().getUserData());
-                 }
-             });
-
-     tooslow.setToggleGroup(group);
-     confusing.setToggleGroup(group);
-     perfect.setToggleGroup(group);
-     toofast.setToggleGroup(group);
-     ok.setToggleGroup(group);
-	*/
+	//Creates an evaluation that can be inserted into the database
+	
+	private void createEvaluation(ActionEvent event){
+		System.out.println(event.getSource());
+		debugText.setText("");
+		if (group.getSelectedToggle() == null) {
+			debugText.setText("Choose a rating");
+			return;
+		}
+	}
+	
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
