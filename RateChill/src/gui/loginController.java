642ed9117b502c9f466af2fce7c5cb21d0d1a305
Nicolai <nicolai_m404@hotmail.com>
2017-02-23 09:24:29 +0100

    started to fix login controller behaviour

--- RateChill/src/gui/loginController.java
+++ RateChill/src/gui/loginController.java
@@ -11,17 +11,21 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
+import databaseobjects.Student;
 public class loginController implements Initializable {
 
 	
 	@FXML
-	Button student;
+	public Button student;
 	@FXML
-	Button professor;
+	public Button professor;
+	public TextField username; 
+	public Text error;
 	
-	String loginInfo;
+	//String loginInfo;
 	
 	@FXML
 	private void handleButtonAction(ActionEvent event) throws IOException{
@@ -31,25 +35,26 @@ public class loginController implements Initializable {
 	    	//get reference to the button's stage
 	    	stage=(Stage) student.getScene().getWindow();
 	    	
-	    	/*
-	    	//here we want to TRY to create a new student object
+	    	//here we want to create a new student object and print error msg if it doesn't exist
 	    	try {
-	    		Student "textfield" = new Student();
+	    		Student stud = new Student(username.getText());
 	    	}
-	    	
-	    	//we want to store the info and use it later, perhaps in a String
-	    	loginInfo = "textfield";
-	    	*/
+	    	catch (Exception e){
+	    		String errorMsg = "User doesn't exist. Try again";
+	    		error.setText(errorMsg);
+	    		throw new IllegalArgumentException(errorMsg);
+	    	}		
 	    }
+	    	
 	    else{
 	    	stage=(Stage) professor.getScene().getWindow();
 	    	/*
-	    	//here we wanna create a new student object if the student does not already exist
+	    	//here we wanna create a new professor object if the professor does not already exist
 	    	if(!("textfield" == "existing professor")) {
 	    		Professor professorObj = new Professor();
 	    	}
 	    	
-	    	//we want to store the info and use it later, perhaps in a String
+	    	//we want to store the info and use it later, perhaps in a String or use a get method?
 	    	loginInfo = "textfield";
 	    	*/
 	    }
