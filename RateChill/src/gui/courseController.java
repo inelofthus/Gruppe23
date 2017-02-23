af88a52ce0ea8dfef953dc7e0c6f569be5252132
Magnus <Magnus@10.22.34.193>
2017-02-23 16:24:42 +0100

    Solved a merge conflinct (hopefully)

--- RateChill/src/gui/courseController.java
+++ RateChill/src/gui/courseController.java
@@ -31,14 +31,13 @@ public class courseController implements Initializable {
 	public Text errorNumber;
 	
 	
-	//an attempt to make a 
-	/*public courseController() {
-		Student currentUser = getCurrentUser();
-	}*/
-	
 	public String loadCourseName(int x) {
 		return mainController.getInstance().getStudents().getCourseIDs().get(x);
 	}
+	public void setLectures (Course course) {
+		mainController.getInstance().setStudentLectureIDs(course.getLastTwoCompletedLectures());
+	}
+	
 	
 	@FXML
 	private void handleButtonAction(ActionEvent event) throws IOException{
@@ -58,29 +57,28 @@ public class courseController implements Initializable {
 	    	//get reference to the button's stage
 	    	Course course = new Course(loadCourseName(0));
 	        stage=(Stage) fag1.getScene().getWindow();
-	        course.getLastTwoCompletedLectures();
+	        setLectures(course);
 	    }
 	    else if (event.getSource()==fag2 && numberOfCourses>1){
 	    	Course course = new Course(loadCourseName(1));
 	    	stage=(Stage) fag2.getScene().getWindow();
-	    	course.getLastTwoCompletedLectures();
+	    	setLectures(course);
 	    }
 	    else if (event.getSource()==fag3 && numberOfCourses>2){
 	    	Course course = new Course(loadCourseName(2));
 	    	stage=(Stage) fag3.getScene().getWindow();
-	    	course.getLastTwoCompletedLectures();
+	    	setLectures(course);
 		}
 	    else if (event.getSource()==fag3 && numberOfCourses>3){
 	    	Course course = new Course(loadCourseName(3));
 	    	stage=(Stage) fag4.getScene().getWindow();
-	    	course.getLastTwoCompletedLectures();
+	    	setLectures(course);
 		}
 	    
 	    //load up OTHER FXML document
         root = FXMLLoader.load(getClass().getResource("lecture.fxml"));
 	    
         
-                
 	    //create a new scene with root and set the stage
 	    Scene scene = new Scene(root);
 	    stage.setScene(scene);
@@ -93,8 +91,8 @@ public class courseController implements Initializable {
 		//System.out.println(mainController.getInstance().getStudents().getCourseIDs().get(0));
 		fag1.setText(loadCourseName(0));
 		fag2.setText(loadCourseName(1));
-		//fag3.setText(mainController.getInstance().getStudents().getCourseIDs().get(2));
-		//fag4.setText(mainController.getInstance().getStudents().getCourseIDs().get(3));
+		fag3.setText(loadCourseName(2));
+		fag4.setText(loadCourseName(3));
 	}
 
 }
