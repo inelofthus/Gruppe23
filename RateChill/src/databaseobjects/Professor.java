3ac480d828347236113e1a0d8ef9c28459e02b95
karim <karim@Fluffy.studby.ntnu.no>
2017-03-06 13:21:24 +0100

    added comments to Professor class

--- RateChill/src/databaseobjects/Professor.java
+++ RateChill/src/databaseobjects/Professor.java
@@ -9,7 +9,7 @@ public class Professor extends DatabaseUser {
 	ArrayList<String> courseIDs;
 	HashMap<String, String> courseIDNames;
 	
-	// Constructor1
+	// Constructor
 	public Professor(String professorUsername) {
 		this.username = professorUsername;
 		DBC.loadProfessorInfo(this);
@@ -20,39 +20,19 @@ public class Professor extends DatabaseUser {
 		return DBC.professorExists(username);
 	}
 	
-	public void loadInfo(){
-		try {
-			courseIDs = DBC.getCoursesTaughtByProfessor(username);
-			
-		} catch (Exception e) {
-			// TODO: handle exception
-			if (!existsInDB()) {
-				throw new NoSuchElementException("No courses exists for this professor");
-			}
-			System.out.println(e.getMessage());
-		}
-	}
-	
+	// Getters
 	public HashMap<String, String> getCourseIDNames() {
 		return courseIDNames;
 	}
 
-	public void setCourseIDNames(HashMap<String, String> courseIDNames) {
-		this.courseIDNames = courseIDNames;
-	}
-
-	public void setCourseIDs(ArrayList<String> courseIDs) {
-		this.courseIDs = courseIDs;
-	}
-
 	public ArrayList<Integer> getLecturesForCourse(String courseCode){
-		// returns lectureIDs for all lectures given by this professor that have already passed in specified course
+		// returns lectureIDs for all lectures given by this professor that have already passed
 		// Order is with the newest lecture first
 		return DBC.getCompletedLecturesForCourseByProfessor(courseCode, username);
 	}
 	
 	public ArrayList<Integer> getLastTwoCompletedLecturesForCourse(String courseCode){
-		// returns lectureIDs for all lectures given by this professor that have already passed in specified course
+		// returns lectureIDs for last two lectures given by this professor that have already passed in specified course
 		// Order is with the newest lecture first
 		ArrayList<Integer> lastTwoCompleteLectures = new ArrayList<>();
 		ArrayList<Integer> allCompletedLectures = DBC.getCompletedLecturesForCourseByProfessor(courseCode, username);
@@ -75,7 +55,34 @@ public class Professor extends DatabaseUser {
 		// will give the corresponding courseName for the students Course
 		return courseIDNames.get(courseCode);
 	}
+	
+	//Setters
+	public void setCourseIDNames(HashMap<String, String> courseIDNames) {
+		this.courseIDNames = courseIDNames;
+	}
 
+	public void setCourseIDs(ArrayList<String> courseIDs) {
+		this.courseIDs = courseIDs;
+	}
+
+	
+	//////////////END OF USEFUL CODE ////////////////////////////
+	
+	//OLD LOAD FUNCTION	
+	
+	/*public void loadInfo(){
+		try {
+			courseIDs = DBC.getCoursesTaughtByProfessor(username);
+			
+		} catch (Exception e) {
+			
+			if (!existsInDB()) {
+				throw new NoSuchElementException("No courses exists for this professor");
+			}
+			System.out.println(e.getMessage());
+		}
+	}*/
+	
 	
 	
 }
