d05d3c9967ddfd569a8473131e358eb6212a2e8a
Magnus <Magnus@10.24.3.214>
2017-03-20 11:49:13 +0100

    Still trying to get commentpage to work, won't work.

--- RateChill/src/gui/commentPageController.java
+++ RateChill/src/gui/commentPageController.java
@@ -16,10 +16,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class commentPageController implements Initializable {
@@ -38,16 +38,66 @@ public class commentPageController implements Initializable {
 	
 	@FXML
 	public ScrollPane scrollPane;
-	public AnchorPane anchor;
+	public Text text = null;
+	//public AnchorPane anchor;
 	public VBox vBox;
     //public TextField textField = new TextField();
 	
     Integer lectureID = mainController.getInstance().getChosenProfessorLecture();
+    ArrayList<Label> labels = new ArrayList<Label>();
 	
 	private Lecture lecture = new Lecture(lectureID);
 	//ArrayList evaluations = lec.getEvaluations();
 	//public ArrayList<String> currEval = selectedButtons();
 	
+	
+	public void setText(Text text) {
+		this.text = text;
+	}
+	
+	public void getText() {
+		ArrayList<String> currentEvaluations = null;
+		if (seePerfectComments.isSelected()){
+			ArrayList<Evaluation> perfEvaluations = lecture.getPerfectEvaluations();
+			for (Evaluation eval:perfEvaluations) {
+				currentEvaluations.add(eval.getComment());
+			}
+		}
+		if (seeOkComments.isSelected()){
+			ArrayList<Evaluation> okEvaluations = lecture.getOkEvaluations();
+			for (Evaluation eval:okEvaluations) {
+				currentEvaluations.add(eval.getComment());
+			}
+		}
+		if (seeFastComments.isSelected()){
+			ArrayList<Evaluation> fastEvaluations = lecture.getTooFastEvaluations();
+			for (Evaluation eval:fastEvaluations) {
+				currentEvaluations.add(eval.getComment());
+			}
+		}
+		if (seeSlowComments.isSelected()){
+			ArrayList<Evaluation> slowEvaluations = lecture.getTooSlowEvaluations();
+			for (Evaluation eval:slowEvaluations) {
+				currentEvaluations.add(eval.getComment());
+			}
+		}
+		if (seeConfusedComments.isSelected()){
+			ArrayList<Evaluation> confusedEvaluations = lecture.getConfusedEvaluations();
+			for (Evaluation eval:confusedEvaluations) {
+				currentEvaluations.add(eval.getComment());
+			}
+		}
+		for (String i:currentEvaluations) {
+			text.setText(text + i);
+		}
+		setText(text);
+		
+		/*for (String string:currentEvaluations) {
+			Label label = new Label();
+			label.setText(string);
+			labels.add(label);
+		}*/
+	}
 	
 	public void loadNextScene(Button button, Stage stage, String string) throws IOException{
 		stage=(Stage) button.getScene().getWindow();
@@ -152,7 +202,12 @@ public class commentPageController implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method
 		
-		//vBox.setPrefSize(anchor.getPrefWidth(), anchor.getPrefHeight());
+		//getText(); //GIVES ERROR
+		//text.wrappingWidthProperty().bind(scrollPane.widthProperty()); //WORKS?
+		VBox.setVgrow(scrollPane, Priority.ALWAYS); //WORKS
+		//vBox.getChildren().addAll(labels); //GIVES ERROR
+		scrollPane.setContent(vBox); //WORKS
+		//vBox.setPrefSize(scrollPane.getPrefWidth(), scrollPane.getPrefHeight()); //GIVES ERROR
 	    
 		
 		//textField.setPrefSize(anchor.getPrefWidth(), anchor.getPrefHeight());
