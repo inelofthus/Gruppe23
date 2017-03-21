056a12e505eda3632373c038bc8e8969463ee26a
karim <karim@10.22.7.218>
2017-03-21 13:40:35 +0100

    added hashPassword method in professor class

--- RateChill/src/databaseobjects/Professor.java
+++ RateChill/src/databaseobjects/Professor.java
@@ -1,4 +1,6 @@
 package databaseobjects;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -77,6 +79,20 @@ public class Professor extends DatabaseUser {
 		this.courseIDs = courseIDs;
 	}
 
+	
+	public static String hashPassword(String password) throws NoSuchAlgorithmException{
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		md.update(password.getBytes());
+		byte[] b = md.digest();
+		StringBuffer sb = new StringBuffer();
+		
+		for(byte b1: b){
+			sb.append(Integer.toHexString(b1 & 0xff).toString());
+		}
+		
+		
+		return sb.toString();
+	}
 	
 	//////////////END OF USEFUL CODE ////////////////////////////
 	
