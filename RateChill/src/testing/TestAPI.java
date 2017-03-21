399973e66e44a11b5b58f15f5b876a6d1b08720e
Ine <inelofthus@gmail.com>
2017-03-21 14:22:30 +0100

    JSON-stuff

---
+++ RateChill/src/testing/TestAPI.java
@@ -0,0 +1,28 @@
+package testing;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class TestAPI {
+	public static void main(String args[]) throws IOException{
+		String sURL = "http://www.ime.ntnu.no/api/course/en/tdt4140"; //just a string
+	
+	    // Connect to the URL using java's native library
+	    URL url = new URL(sURL);
+	    HttpURLConnection request = (HttpURLConnection) url.openConnection();
+	    request.connect();
+	
+	    // Convert to a JSON object to print data
+	    JsonParser jp = new JsonParser(); //from gson
+	    JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
+	    JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object. 
+	    System.out.println(rootobj.get("course").getAsString()); //just grab the zipcode
+	}
+}
