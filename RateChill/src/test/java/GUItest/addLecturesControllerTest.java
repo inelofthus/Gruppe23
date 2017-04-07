4667b8a3d16e7724f75412d4534a5cff5d569810
Nicolai <nicolai_m404@hotmail.com>
2017-04-07 15:13:57 +0200

    gui test package

---
+++ RateChill/src/test/java/GUItest/addLecturesControllerTest.java
@@ -0,0 +1,17 @@
+package GUItest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class addLecturesControllerTest {
+ 
+	
+	@Test
+	public void testValidate() {
+		
+		
+		
+	}
+
+}
