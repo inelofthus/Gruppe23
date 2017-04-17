737b09da444eea134fd88fd1d0668056edeea43e
karim <karim@Fluffy.studby.ntnu.no>
2017-04-17 18:31:41 +0200

    deleted unused code

--- RateChill/src/main/java/gui/PopupController.java
+++ RateChill/src/main/java/gui/PopupController.java
@@ -1,14 +1,10 @@
 package gui;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.text.Text;
@@ -20,17 +16,6 @@ public class PopupController implements Initializable {
 	public Button closeButton;
 	public Text title;
 	public Label message;	
-	
-	public void loadNextScene(Button button, Stage stage, String string) throws IOException{
-		stage=(Stage) button.getScene().getWindow();
-		Parent root;
-		root = FXMLLoader.load(getClass().getResource(string));
-		
-		//create a new scene with root and set the stage
-		Scene scene = new Scene(root);
-		stage.setScene(scene);
-		stage.show();
-	}
 	
 	public void handleButtonAction(ActionEvent event){
 		if(event.getSource() == closeButton){
