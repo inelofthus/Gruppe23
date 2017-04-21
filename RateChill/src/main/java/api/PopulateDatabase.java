ac14fdc95fc9ad4483876b240a0c5bf42f2d8e34
Ine <inelofthus@gmail.com>
2017-04-21 12:00:26 +0200

    Added professors to comment about what gets inserted into the database

--- RateChill/src/main/java/api/PopulateDatabase.java
+++ RateChill/src/main/java/api/PopulateDatabase.java
@@ -12,6 +12,6 @@ import java.io.IOException;
 public class PopulateDatabase {
 	public static void main(String[] args) throws IOException {
 		IME_API ime = new IME_API();
-		ime.getApiInfo(); //Updates courses and lectures in database
+		ime.getApiInfo(); //Updates courses, professors and lectures in database
 	}
 }
