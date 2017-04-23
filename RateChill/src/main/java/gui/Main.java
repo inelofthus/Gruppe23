f07e4c8a62750de47691e86b3cd7b91958f0ebd5
karim <karim@Fluffy.studby.ntnu.no>
2017-04-23 15:18:18 +0200

    code review for Main

--- RateChill/src/main/java/gui/Main.java
+++ RateChill/src/main/java/gui/Main.java
@@ -6,12 +6,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Main --- The Main class opens the application and loads the first GUI
+ * 
+ * @author Group 23: Ine Lofthus Arnesen, Kari Meling Johannessen, Nicolai
+ *         Cappelen Michelet, Magnus Tvilde
+ */
 public class Main extends Application {
 
 	public static void main(String[] args) {
 		launch(Main.class,args);
 	}
-
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		final FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
